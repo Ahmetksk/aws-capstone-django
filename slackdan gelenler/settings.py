661a43ed0d5fd4ac86e7c691910b6211e81b2b5f
ahmetksk <ahmetturfan@gmail.com>
2023-12-13 00:36:45 +0000

    third

--- slackdan gelenler/settings.py
+++ slackdan gelenler/settings.py
@@ -83,8 +83,8 @@ def get_ssm_parameters():
     ssm = boto3.client('ssm', region_name='us-east-1')
 
     # AWS SSM Parametr define
-    username_param = ssm.get_parameter(Name='/ahmet/capstone/username')
-    password_param = ssm.get_parameter(Name="/ahmet/capstone/password", WithDecryption=True)
+    username_param = ssm.get_parameter(Name='/osvaldo/capstone/username')
+    password_param = ssm.get_parameter(Name="/osvaldo/capstone/password", WithDecryption=True)
 
 
     # Parametre retrieve
@@ -102,7 +102,7 @@ DATABASES = {
         'NAME': 'clarusway',
         'USER': db_username,
         'PASSWORD': db_password,
-        'HOST': 'aws-capstone-rds.cxkoew7oeawm.us-east-1.rds.amazonaws.com',
+        'HOST': 'capstone.cbanmzptkrzf.us-east-1.rds.amazonaws.com',
         'PORT': '3306',
     }
 }
@@ -156,7 +156,7 @@ LOGIN_REDIRECT_URL = "blog:list"
 LOGIN_URL = "login"
 
 
-AWS_STORAGE_BUCKET_NAME = 'ahmetcapstonestatic' # please enter your s3 bucket name
+AWS_STORAGE_BUCKET_NAME = 'osvaldocapstonestatic' # please enter your s3 bucket name
 AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
 AWS_S3_REGION_NAME = "us-east-1" # please enter your s3 region 
 AWS_DEFAULT_ACL = 'public-read'
